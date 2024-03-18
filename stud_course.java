4f4fd1f1304e5c9d60bf1f7f6131ac85ba0adc6d
01fe22bca146 <01fe22bca146@kletech.ac.in>
2024-03-18 08:37:03 +0530

    new codes

---
+++ stud_course.java
@@ -0,0 +1,25 @@
+ class Stud_Course extends student {
+    void course_det()
+{
+    String[] subjects = {"DevOps", "Machine Learning", "C#"};
+    int[][] marks = {
+        {50, 30, 60}, 
+        {70, 45, 55}, 
+        {35, 40, 50}  
+    };
+    for (int i = 0; i < marks.length; i++) {
+        for (int j = 0; j < marks[i].length; j++) {
+            if (marks[i][j] < 40) {
+                System.out.println("Student " + (i + 1) + " scored less than 40 in " + subjects[j]);
+            }
+        }
+    }
+
+}
+public static void main(String args[])
+{
+    Stud_Course s1 = new Stud_Course();
+    s1.stud_det();
+    s1.course_det();
+}
+}
